b1d2d48bcb7dd230d4784010338a8fe02e2e3b65
adeegenie <asultan@egenienext.com>
2019-09-18 19:50:37 +0500

    Web Folder Changes

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/broadcasts/view/{id}', 'BroadcastsController@view')->name('view_broadcast');
+Route::get('/broadcasts/view/{id}', 'Web/BroadcastsController@view')->name('view_broadcast');
