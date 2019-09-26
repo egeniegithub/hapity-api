58e0ac0c095d59c4a8e37a5d516f405a281ab634
Hamid Ashraf <hamid.ashraf@egenie.local>
2019-09-27 01:00:47 +0500

    Fixed issues with login

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder
 {
@@ -14,10 +14,10 @@ class UserTableSeeder extends Seeder
     {
         $user = new User();
 
-        $user->name =  'Super Admin';
-        $user->email =  'administrator@example.com';
-        $user->username =  'superadmin';
-        $user->password =  bcrypt('admin125');
+        $user->name = 'Super Admin';
+        $user->email = 'administrator@example.com';
+        $user->username = 'superadmin';
+        $user->password = bcrypt('admin125');
         $user->save();
 
         $user->roles()->attach('1');
