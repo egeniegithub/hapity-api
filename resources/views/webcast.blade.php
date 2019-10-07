4a27074adcb1262129995976b0db7261580f4b46
fahimegenie <mfahim@egenienext.com>
2019-10-07 15:19:29 +0500

    replace url to routes all pages

--- resources/views/webcast.blade.php
+++ resources/views/webcast.blade.php
@@ -125,7 +125,8 @@
             reader.onloadend = function() {
                 console.log('RESULT', reader.result);
                 bd_image = reader.result;
-                $('.live-streaming').css("background-image", "url('" + bd_image + "')");
+                
+                $('.live-streaming').css("background-image", "asset('" + bd_image + "')");
 
                 if($('.upload-custom-image').hasClass('active-this-btn')){
                     $('.upload-custom-image').hide().removeClass('.active-this-btn');
