06e3d29150ff561dadfb9adfed7b301c66241e69
fahimegenie <mfahim@egenienext.com>
2019-10-09 20:57:40 +0500

    add text in 500.blade.php page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,4 +1,20 @@
-@extends('errors::minimal')
+{{-- @extends('errors::minimal') --}}
+
+@extends('layouts.app')
+@section('content')
+<div class="profile-page new_design">
+    <div class="section-main">
+      <div class="container">        
+        <div class="about-ContactForm-wrapepr-new" style="padding: 50px; margin: 150px auto;">
+          <h2 class="text-center">Opps... Something went wrong!</h2>          
+        </div>
+      </div>
+    </div>
+  
+  
+  </div>
+  <div class="clear"> <p><br><br></p></div>
+  @endsection
 
 @section('title', __('Server Error'))
 @section('code', '500')
