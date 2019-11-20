4732586a8646b597c7f30c208c19ab151e68848a
fahimegenie <mfahim@egenienext.com>
2019-11-20 23:02:06 +0500

    add ffmpeg exec logs in log file

--- app/Http/Helpers/FFMPegFileUploadPath.php
+++ app/Http/Helpers/FFMPegFileUploadPath.php
@@ -1,6 +1,8 @@
 <?php
 
-  function ffmpeg_upload_file_path($source_file_path){
+use Illuminate\Support\Facades\Log;
+
+function ffmpeg_upload_file_path($source_file_path){
     $temp_pathtosave =  storage_path('temp');
 
     if(file_exists($source_file_path)) {
@@ -11,12 +13,23 @@
         $temp_file_path = $temp_pathtosave . DIRECTORY_SEPARATOR . $file_name;
 
         $shell_exec = shell_exec('cp ' . $source_file_path . ' ' . $temp_file_path);
-        
+
+        Log::channel('ffmpeg_logs')->info('shell_exec(cp '.$shell_exec);
+
         $shell_exec = shell_exec("ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 $temp_pathtosave");
+        
+        Log::channel('ffmpeg_logs')->info('shell_exec(ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 '.$shell_exec);
+
         if($shell_exec == 90){
             unlink($source_file_path);
             $shell_exec = shell_exec('ffmpeg -i "'.$temp_pathtosave.'" -vf "transpose=1,transpose=2" '. $source_file_path);
-            shell_exec('rm '.$temp_pathtosave);
+            
+            Log::channel('ffmpeg_logs')->info('shell_exec(ffmpeg -i '.$shell_exec);
+
+            $shell_exec = shell_exec('rm '.$temp_pathtosave);
+
+            Log::channel('ffmpeg_logs')->info('shell_exec(rm '.$shell_exec);
+
         }
     }
 }
