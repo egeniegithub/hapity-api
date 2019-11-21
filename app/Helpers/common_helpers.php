1959148186b0482588cf55020e3fe8063f017f4f
Hamid Ashraf <hamid.ashraf@egenie.local>
2019-11-21 18:12:45 +0500

    Modified FFMPEG Script

--- app/Helpers/common_helpers.php
+++ app/Helpers/common_helpers.php
@@ -5,13 +5,14 @@ use Illuminate\Support\Facades\Log;
 if (!function_exists('ffmpeg_upload_file_path')) {
     function ffmpeg_upload_file_path($source_file_path)
     {
-        $temp_pathtosave = storage_path('temp');
+        
 
         if (file_exists($source_file_path)) {
 
-            $file_name = basename($source_file_path);
+            $file_name = pathinfo($source_file_path, PATHINFO_BASENAME);
+            $directory_name = pathinfo($source_file_path, PATHINFO_DIRNAME);
 
-            $temp_file_path = $temp_pathtosave . DIRECTORY_SEPARATOR . $file_name;
+            $temp_file_name = 'temp-' . $file_name;
 
             $ffprobe_command = "ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 \"$source_file_path\"";
             $shell_exec = shell_exec($ffprobe_command);
@@ -20,10 +21,10 @@ if (!function_exists('ffmpeg_upload_file_path')) {
             Log::channel('ffmpeg_logs')->info('ffprobe Result:  ' . $shell_exec);
 
             if (intval($shell_exec) == 90) {
-                $commands = 'cp "' . $source_file_path . '" "' . $temp_file_path . '"' . PHP_EOL;
-                $commands .= 'rm "' . $source_file_path . '"' . PHP_EOL;
-                $commands .= 'ffmpeg -i "' . $temp_file_path . '" -vf "transpose=1,transpose=2" "' . $source_file_path . '"' . PHP_EOL;
-                $commands .= 'rm "' . $temp_file_path . '"' . PHP_EOL;
+                $commands = '';
+                $commands .= 'cp "' . $source_file_path . '" "' . $directory_name . DIRECTORY_SEPARATOR . $temp_file_name . '"' . PHP_EOL ;
+                $commands .= 'ffmpeg -y -i "' . $temp_file_name . '" -vf "transpose=1,transpose=2" "' . $source_file_path . '"' . PHP_EOL;
+                $commands .= 'rm "' . $directory_name . DIRECTORY_SEPARATOR . $temp_file_name . '"' . PHP_EOL ; 
                 
                 $shell_exec = shell_exec($commands);
                 Log::channel('ffmpeg_logs')->info('Start ============================================================================================================');
