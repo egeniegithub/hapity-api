32b042d915379e2902a95c2159e667f0bafa9dd5
Hamid Ashraf <hamid.ashraf@egenie.local>
2019-11-21 16:40:56 +0500

    code refactor

--- app/Helpers/common_helpers.php
+++ app/Helpers/common_helpers.php
@@ -11,30 +11,31 @@ if (!function_exists('ffmpeg_upload_file_path')) {
 
             $file_name = basename($source_file_path);
 
-            $temp_file_path = $temp_pathtosave . DIRECTORY_SEPARATOR . $file_name;            
+            $temp_file_path = $temp_pathtosave . DIRECTORY_SEPARATOR . $file_name;
 
-            $ffprobe_command = "ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 $source_file_path";
+            $ffprobe_command = "ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate -of default=nw=1:nk=1 \"$source_file_path\"";
             $shell_exec = shell_exec($ffprobe_command);
 
-            Log::channel('ffmpeg_logs')->info('ffprobe command:  ' . $ffprobe_command);
-            Log::channel('ffmpeg_logs')->info('ffprobe command Result:  ' . $shell_exec);
+            Log::channel('ffmpeg_logs')->info('ffprobe:  ' . $ffprobe_command);
+            Log::channel('ffmpeg_logs')->info('ffprobe Result:  ' . $shell_exec);
 
             if ($shell_exec == 90) {
-                $copy_command = 'cp ' . $source_file_path . ' ' . $temp_file_path;
+                $copy_command = 'cp "' . $source_file_path . '" "' . $temp_file_path . '"';
                 $shell_exec = shell_exec($copy_command);
-                Log::channel('ffmpeg_logs')->info('command :   ' . $copy_command);
+                Log::channel('ffmpeg_logs')->info('Copy:   ' . $copy_command);
 
                 unlink($source_file_path);
 
                 $ffmpeg_command = 'ffmpeg -i "' . $temp_file_path . '" -vf "transpose=1,transpose=2" ' . $source_file_path;
                 $shell_exec = shell_exec($ffmpeg_command);
 
-                Log::channel('ffmpeg_logs')->info('ffmpeg command :  ' . $ffmpeg_command);
+                Log::channel('ffmpeg_logs')->info('ffmpeg: ' . $ffmpeg_command);
 
-                $remove_command = 'rm ' . $temp_file_path;
+                $remove_command = 'rm "' . $temp_file_path . '"';
                 $shell_exec = shell_exec($remove_command);
 
-                Log::channel('ffmpeg_logs')->info('rm command :  ' . $remove_command);
+                Log::channel('ffmpeg_logs')->info('rm:  ' . $remove_command);
+                Log::channel('ffmpeg_logs')->info('============================================================================================================');
             }
         }
     }
