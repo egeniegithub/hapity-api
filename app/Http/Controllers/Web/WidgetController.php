bfd6db0ada0bd036b85f139e00943dca513edbed
fahimegenie <mfahim@egenienext.com>
2019-11-28 19:09:16 +0500

    add image in wowza video player widget file

--- app/Http/Controllers/Web/WidgetController.php
+++ app/Http/Controllers/Web/WidgetController.php
@@ -9,37 +9,31 @@ class WidgetController extends Controller
 {
     
     public function index(Request $request){
+    	$data['b_description'] = '';
     	if(isset($request['stream'])){
-	    	$data['b_id'] = isset($request['bid']) ? $request['bid'] : '';       
-	        if($request['title']){
-	            $data['b_title'] = $request['title'];
-	        } else {
-	            $data['b_title'] = "Untitled";
-	        }
+	    	$data['b_id'] = isset($request['bid']) ? $request['bid'] : '';  
+
+	        $data['b_title'] = isset($request['title']) ? $request['title'] : 'Untitled';
+	        $data['b_description  '] = isset($request['description']) ? $request['description'] : '';
 
 	        if(isset($request['broadcast_image']) && $request['broadcast_image']!=''){
-			        $data['image'] = $request['broadcast_image'];
+			        $data['broadcast_image'] = $request['broadcast_image'];
 		    }else{
-		        $data['image'] = public_path('images/default001.jpg');
+		        $data['broadcast_image'] = public_path('images/default001.jpg');
 		    }
+		    $stream = $request['stream'];
+		    $stream = str_replace("rtsp", "rtmp", $stream);
+		    // Temporary change IP address
+		    $stream = str_replace("52.17.132.36", "52.18.33.132", $stream);
+		    // Remove Port
+		    $stream = str_replace(":1935", "", $stream);
+		    // Replace Ip Address With SubDomain
+		    $stream = str_replace(["52.17.132.36", "52.18.33.132"], 'media.hapity.com', $stream);
+		    if($request['status'] != 'online'){
+		        $stream = str_replace("live", "vod", $stream);
+		    }
+		    $data['stream_url'] = str_replace(array("rtmp", "rtsp"), "https", $stream).'/playlist.m3u8';
 
-	        $file_info = pathinfo($request['stream']);
-
-	        $file_ext = isset($file_info['extension']) ? $file_info['extension'] : 'mp4';
-
-	        $data['b_description'] = isset($request['description']) ? $request['description'] : '';
-
-	        $vod_app = env('APP_ENV') == 'staging' ? 'stage_vod' : 'vod';
-	        $live_app = env('APP_ENV') == 'staging' ? 'stage_live' : 'live';
-	        $vod_app = 'stage_vod';
-	        $live_app = 'stage_live';
-
-	        $stream_url = urlencode('https://media.hapity.com/' . $vod_app .  '/_definst_/' . $file_ext . ':' .  $request['stream'] . '/playlist.m3u8') ;
-	        if($request['status'] == 'online') {
-	            $file = pathinfo($request['stream'], PATHINFO_FILENAME );                                    
-	            $stream_url = urlencode('https://media.hapity.com/' . $live_app . '/' .  $file . '/playlist.m3u8') ;
-	        }
-	       $data['stream_url'] = $stream_url;
 
 		    return view('widget.widget',$data);
 		}
