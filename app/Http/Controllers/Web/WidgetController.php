7512581710fcf6c2d9ee616d02edb5671d3905ec
fahimegenie <mfahim@egenienext.com>
2019-12-03 17:45:58 +0500

    fix image url for posting plugin

--- app/Http/Controllers/Web/WidgetController.php
+++ app/Http/Controllers/Web/WidgetController.php
@@ -15,17 +15,22 @@ class WidgetController extends Controller
     	if(isset($request['stream'])){
     		$user_id = isset($request['user_id']) ? $request['user_id'] : '0';
 
-	    	$data['b_id'] = isset($request['bid']) ? $request['bid'] : '';  
-
+			$data['b_id'] = isset($request['bid']) ? $request['bid'] : '';  
+			$broadcast = Broadcast::find($data['b_id']);
+            if(!is_null($broadcast)){
+				$status = $broadcast->status;
+				$user_id = $broadcast->user_id;
+        	}
+		
 	        $data['b_title'] = isset($request['title']) ? $request['title'] : 'Untitled';
 	        $data['b_description  '] = isset($request['description']) ? $request['description'] : '';
 
-	        if(isset($request['broadcast_image']) && $request['broadcast_image']!=''){
+	        if($user_id > 0 && isset($request['broadcast_image']) && $request['broadcast_image']!=''){
 			        $data['broadcast_image'] = asset('images/broadcasts/'.$user_id.'/'.$request['broadcast_image']);
 		    }else{
 		        $data['broadcast_image'] = asset('images/default001.jpg');
 		    }
-
+			
 		    $file_info = pathinfo($request['stream']);
 
             $file_ext = isset($file_info['extension']) ? $file_info['extension'] : 'mp4';
@@ -35,10 +40,7 @@ class WidgetController extends Controller
 
             $stream_url = urlencode('https://media.hapity.com/' . $vod_app .  '/_definst_/' . $file_ext . ':' .  $request['stream'] . '/playlist.m3u8') ;
             $status = $request['status'];
-            $broadcast = Broadcast::find($data['b_id']);
-            if(!is_null($broadcast)){
-            	$status = $broadcast->status;
-        	}
+            
 
             if($status == 'online') {
                 $file = pathinfo($request['stream'], PATHINFO_FILENAME );                                    
