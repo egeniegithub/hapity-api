388667b3a3a2af98f93626dc8d8698ab879b0fea
Hamid Ashraf <hashraf@egenienext.com>
2019-12-04 23:03:52 +0500

    Fixed redirect

--- public/assets/handle_redirect_widget_file.php
+++ public/assets/handle_redirect_widget_file.php
@@ -1,33 +1,30 @@
 
 <?php
-    if(isset($_GET['stream'])){
-        if(isset($_GET['broadcast_image']) && $_GET['broadcast_image']!=''){
-            $image = urldecode($_GET['broadcast_image']);
-        }
-        else{
-            $image = 'default001.jpg';
-        }
-        $stream = $_GET['stream'];
-        $title = isset($_GET['title']) ? urldecode($_GET['title']) : 'Untitle';
-        $description = isset($_GET['description']) ? $_GET['description'] : '';
-        $id = isset($_GET['bid']) ? $_GET['bid'] : 0;
-        $status = isset($_GET['status']) ? urldecode($_GET['status']) : 'offline';
-        $stream = urldecode($_GET['stream']);
-        $stream = str_replace('/playlist.m3u8', '', $stream);
-        if(str_replace('mp4:', '', $stream)){
-            $stream = str_replace('mp4:', '', $stream);
-        }
-
-        $data = pathinfo($stream);
-
-        $file_name = $data['basename'];
-
-        $host_name = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-
-        $widget = "$host_name/widget?stream=$file_name&title=$title&status=$status&bid=$id&broadcast_image=$image&description=$description";
-
-        header("Location: $widget");
-        
+if (isset($_GET['stream'])) {
+    if (isset($_GET['broadcast_image']) && $_GET['broadcast_image'] != '') {
+        $image = urldecode($_GET['broadcast_image']);
+    } else {
+        $image = 'default001.jpg';
     }
- 
-   
+    $stream = $_GET['stream'];
+    $title = isset($_GET['title']) ? urldecode($_GET['title']) : 'Untitle';
+    $description = isset($_GET['description']) ? $_GET['description'] : '';
+    $id = isset($_GET['bid']) ? $_GET['bid'] : 0;
+    $status = isset($_GET['status']) ? urldecode($_GET['status']) : 'offline';
+    $stream = urldecode($_GET['stream']);
+    $stream = str_replace('/playlist.m3u8', '', $stream);
+    if (str_replace('mp4:', '', $stream)) {
+        $stream = str_replace('mp4:', '', $stream);
+    }
+
+    $data = pathinfo($stream);
+
+    $file_name = $data['basename'];
+
+    $host_name = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://www.hapity.com";
+
+    $widget = "$host_name/widget?stream=$file_name&title=$title&status=$status&bid=$id&broadcast_image=$image&description=$description";
+
+    header("Location: $widget");
+
+}
