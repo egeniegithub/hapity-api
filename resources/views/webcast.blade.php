8af4871cab437933345cfc1a9af300dac3439839
Hamid Ashraf <hashraf@egenienext.com>
2019-12-12 21:07:51 +0500

    remove unwanted &

--- resources/views/webcast.blade.php
+++ resources/views/webcast.blade.php
@@ -90,7 +90,7 @@
                                 <param name="allowFullScreen" value="true" />
                                 <param name="allowScriptAccess" value="" />
                                 <param name="flashVars" 
-                                value="controls=true&live=true&url=rtmp://52.18.33.132:1935/live/{{ $time. '.stream' }}&publish={{ $time. '.stream' }}&record=true&cameraQuality=100&videoCodec=H264Avc&" />
+                                value="controls=true&live=true&url=rtmp://52.18.33.132:1935/live/{{ $time. '.stream' }}&publish={{ $time. '.stream' }}&record=true&cameraQuality=100&videoCodec=H264Avc" />
                             </object>
                         </div>
                     </div>
