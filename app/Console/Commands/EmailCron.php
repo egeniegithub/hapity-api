58339ef6f6cf5bfea22904503c7e29f4b1fd399c
fahimegenie <mfahim@egenienext.com>
2019-12-13 21:58:00 +0500

    add schedule cron testing cron

---
+++ app/Console/Commands/EmailCron.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class EmailCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info("Cron is working fine!");
+        $email = "fahim.egenie@gmail.com";
+        Mail::send('emails/test_mail', ['data' => 'etesting email'], function ($message) use ($email) {
+            $message->to($email,'chris@hapity.com')->subject('Welcome');
+        });
+
+        $this->info('email:cron Command Run successfully!');
+    }
+}
