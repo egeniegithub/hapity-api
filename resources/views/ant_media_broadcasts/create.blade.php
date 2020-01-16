7c3c31fcf7691482a5bd7fd7daba938411b1a644
Hamid Ashraf <hashraf@egenienext.com>
2020-01-16 19:53:41 +0500

    comment error handlers for debug

--- resources/views/ant_media_broadcasts/create.blade.php
+++ resources/views/ant_media_broadcasts/create.blade.php
@@ -330,6 +330,7 @@
             callbackError : function(error, message) {
                 //some of the possible errors, NotFoundError, SecurityError,PermissionDeniedError
                 
+                /*
                 console.log("error callback: " +  JSON.stringify(error));
                 var errorMessage = JSON.stringify(error);
                 if (typeof message != "undefined") {
@@ -351,10 +352,11 @@
                 else if (error.indexOf("TypeError") != -1) {
                     errorMessage = "Video/Audio is required";
                 }
+                */
             
-                alertify.error(errorMessage);
-
-                $('#start_publish_button').prop('disabled', true);
+                //alertify.error(errorMessage);
+                console.log(error, message);
+                
             }
         });
     </script>
