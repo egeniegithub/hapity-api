af69bf94131d46860a96cc1b3c23246120114e10
Hamid Ashraf <hashraf@egenienext.com>
2020-01-21 16:00:05 +0500

    Curl to check for domain existance

--- app/Broadcast.php
+++ app/Broadcast.php
@@ -3,24 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Broadcast extends Model
 {
 
     protected $table = 'broadcasts';
     protected $guarded = [];
-    
+
     public function user()
     {
         return $this->belongsTo('App\User', 'user_id');
     }
 
-    public function likes() {
+    public function likes()
+    {
         return $this->belongsToMany('App\User', 'broadcast_likes', 'broadcast_id', 'user_id');
     }
 
-    public function comments() {
+    public function comments()
+    {
         return $this->belongsToMany('App\User', 'broadcast_comments', 'broadcast_id', 'user_id');
     }
 
@@ -28,10 +29,12 @@ class Broadcast extends Model
     {
         return $this->hasMany('App\BroadcastComment', 'broadcast_id');
     }
-    public function reportedBradcast(){
-        return $this->hasMany('App\ReportBroadcast','broadcast_id')->select('broadcast_id');
-    } 
-    public function userWithReportedUser(){
+    public function reportedBradcast()
+    {
+        return $this->hasMany('App\ReportBroadcast', 'broadcast_id')->select('broadcast_id');
+    }
+    public function userWithReportedUser()
+    {
         return $this->belongsTo('App\User', 'user_id')->with('reportedUser');
     }
 
