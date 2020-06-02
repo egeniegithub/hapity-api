a609d0e7e35de7d03a89be601ca2eed5fa7ab3e7
Saleem <msaleem@egenienext.com>
2020-06-02 16:08:58 +0500

    deployment changes

--- config/constants.php
+++ config/constants.php
@@ -18,7 +18,11 @@ switch (env('APP_URL')) {
 		# code...
 		break;
 }
-define('ANTMEDIA_HOST', 'stg-media.hapity.com');
+if(env('APP_ENV') == 'live')
+	define('ANTMEDIA_HOST', 'antmedia.hapity.com');
+else
+	define('ANTMEDIA_HOST', 'stg-media.hapity.com');
+
 define('ANT_MEDIA_SERVER_STAGING_IP', 'http://34.255.219.25:5080/');
 define('ANT_MEDIA_SERVER_STAGING_URL', 'https://'.ANTMEDIA_HOST.':5443/');
 
