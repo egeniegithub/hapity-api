d9c4586f6dae00c52d379a0729e3c84292add026
Saleem <msaleem@egenienext.com>
2020-06-02 15:30:40 +0500

    deployment changes

--- app/Http/Controllers/Web/AntMediaBroadcastsController.php
+++ app/Http/Controllers/Web/AntMediaBroadcastsController.php
@@ -309,7 +309,7 @@ class AntMediaBroadcastsController extends Controller
                 'file_name' => $output_file_name,
                 'file_path' => $antmedia_path . DIRECTORY_SEPARATOR . $output_file_name,
                 'file_stream_url' => $stream_url,
-                'file_server' => env('ANTMEDIA_URL'),
+                'file_server' => 'https://antmedia.hapity.com:5443/',
             ];
         }
 
--- config/constants.php
+++ config/constants.php
@@ -20,6 +20,6 @@ switch (env('APP_URL')) {
 }
 
 define('ANT_MEDIA_SERVER_STAGING_IP', 'http://34.255.219.25:5080/');
-define('ANT_MEDIA_SERVER_STAGING_URL', env('ANTMEDIA_URL'));
+define('ANT_MEDIA_SERVER_STAGING_URL', 'https://antmedia.hapity.com:5443/');
 
 define('WEBRTC_APP', 'WebRTCAppEE');
