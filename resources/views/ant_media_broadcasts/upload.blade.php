50b233d0453b2eaf94756f5e606bcf45fda5f590
Saleem <msaleem@egenienext.com>
2020-06-04 18:29:21 +0500

    browser detect issue fixed

--- resources/views/ant_media_broadcasts/upload.blade.php
+++ resources/views/ant_media_broadcasts/upload.blade.php
@@ -111,7 +111,50 @@
     <script src="{{ asset('assets/webrtc/adapter-latest.js') }}"></script>
     <script src="{{ asset('assets/webrtc/webrtc_adaptor.js') }}"></script>
     <script>
-        $(document).ready(function(){   
+        $(document).ready(function(){
+            function checkbrowser() { 
+                var navUserAgent = navigator.userAgent;
+                var browserName  = navigator.appName;
+                var browserVersion  = ''+parseFloat(navigator.appVersion); 
+                var majorVersion = parseInt(navigator.appVersion,10);
+                var tempNameOffset,tempVersionOffset,tempVersion;
+
+
+                if ((tempVersionOffset=navUserAgent.indexOf("Opera"))!=-1) {
+                browserName = "Opera";
+                browserVersion = navUserAgent.substring(tempVersionOffset+6);
+                if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1) 
+                browserVersion = navUserAgent.substring(tempVersionOffset+8);
+                } else if ((tempVersionOffset=navUserAgent.indexOf("MSIE"))!=-1) {
+                browserName = "Microsoft Internet Explorer";
+                browserVersion = navUserAgent.substring(tempVersionOffset+5);
+                } else if ((tempVersionOffset=navUserAgent.indexOf("Chrome"))!=-1) {
+                browserName = "Chrome";
+                browserVersion = navUserAgent.substring(tempVersionOffset+7);
+                } else if ((tempVersionOffset=navUserAgent.indexOf("Safari"))!=-1) {
+                browserName = "Safari";
+                browserVersion = navUserAgent.substring(tempVersionOffset+7);
+                if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1) 
+                browserVersion = navUserAgent.substring(tempVersionOffset+8);
+                } else if ((tempVersionOffset=navUserAgent.indexOf("Firefox"))!=-1) {
+                browserName = "Firefox";
+                browserVersion = navUserAgent.substring(tempVersionOffset+8);
+                } else if ( (tempNameOffset=navUserAgent.lastIndexOf(' ')+1) < (tempVersionOffset=navUserAgent.lastIndexOf('/')) ) {
+                browserName = navUserAgent.substring(tempNameOffset,tempVersionOffset);
+                browserVersion = navUserAgent.substring(tempVersionOffset+1);
+                if (browserName.toLowerCase()==browserName.toUpperCase()) {
+                browserName = navigator.appName;
+                }
+                }
+
+                // trim version
+                if ((tempVersion=browserVersion.indexOf(";"))!=-1)
+                browserVersion=browserVersion.substring(0,tempVersion);
+                if ((tempVersion=browserVersion.indexOf(" "))!=-1)
+                browserVersion=browserVersion.substring(0,tempVersion);
+                return browserName+" "+browserVersion;
+
+            }
             $('#image_upload_loader').hide();
             $('#video_upload_loader').hide();
             $("#update_button").attr("disabled", true);
@@ -119,7 +162,8 @@
                 if($('#broadcast_form').valid()) {                    
 
                     var form_data = $('#broadcast_form').serialize();
-                    form_data += "&meta_info=" + navigator.userAgent;
+                    form_data += "&meta_info=" + checkbrowser();
+                    
                     var my_request;
                     my_request = $.ajax({
                         url: "{{ route('broadcasts.ajax') }}",
